4a1ce3acab75292cc8dc71bec2c8696a98e9f621
vudongkm1997 <vudongkm1997@gmail.com>
2019-01-31 14:30:17 +0700

    frontend: detail exam view

--- AngularUI/src/app/exam/detail-exam/detail-exam.component.ts
+++ AngularUI/src/app/exam/detail-exam/detail-exam.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { mergeMap } from 'rxjs/operators';
+import { DetailExam } from './detailExam.interface';
 
 @Component({
   selector: 'app-detail-exam',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-exam.component.css']
 })
 export class DetailExamComponent implements OnInit {
+  detailExam: DetailExam;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.pipe(
+      mergeMap(
+        params => {
+          const id = params.get('id');
+          return this.http.get<DetailExam>(`http://localhost:3000/exams/${id}`);
+        }
+      )
+    ).subscribe(detailExam => {
+      this.detailExam = detailExam;
+      console.log(this.detailExam);
+    });
   }
 
 }
