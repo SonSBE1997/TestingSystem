c7b9a47f3d4fd0c2373c4fc4cce77ecf16606b57
ptp123 <42292741+ptp123@users.noreply.github.com>
2019-02-15 16:49:50 +0700

    push ending...

--- AngularUI/src/app/exam/create-exam/create-exam.component.ts
+++ AngularUI/src/app/exam/create-exam/create-exam.component.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Exam } from 'src/app/entity/Exam.interface';
 import { v4 as uuid } from 'uuid';
+import { Category } from '../../entity/Category.interface';
+import { User } from 'src/app/entity/User.interface';
+import { ExamQuestion } from 'src/app/entity/ExamQuestion.interface';
 
 @Component({
   selector: 'app-create-exam',
@@ -15,28 +18,47 @@ import { v4 as uuid } from 'uuid';
 export class CreateExamComponent implements OnInit {
   public Editor = ClassicEditor;
   examFrm: FormGroup;
+  categories: Category[] = [];
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
     private router: Router
   ) {}
     ngOnInit() {
+      this.http.get<Category[]>('http://localhost:8080/category/listCategories').subscribe(category => {
+        this.categories = category;
+      });
       this.examFrm = this.fb.group({
       title: ['', Validators.required],
-      categoryName: ['', Validators.required],
+      category: ['', Validators.required],
       numberquestion: ['', Validators.required],
       duration: ['', Validators.required],
-      status: ['', Validators.required],
       note: ['', Validators.required],
     });
   }
   onCreate() {
-    const data = this.examFrm.value;
+    const user: User = null;
+    const examQuestions: ExamQuestion[] = [];
+    const date: Date = null ;
+    const cate: Category = this.examFrm.get('category').value;
       const exam: Exam = {
         examId: uuid(),
-        ...data
+        title: this.examFrm.get('title').value,
+        duration: this.examFrm.get('duration').value,
+        note: this.examFrm.get('note').value,
+        status: 'Draft',
+        categoryName: cate.categoryName,
+        createAt: date,
+        modifiedAt: date,
+        numberOfQuestion: this.examFrm.get('numberquestion').value,
+        category: cate,
+        userCreated: user,
+        modifiedBy:  user,
+        examQuestions: examQuestions,
+        enable: false,
       };
-      console.log(data);
+      console.log(exam);
+      console.log(cate);
       this.http
       .post('http://localhost:8080/exam/create', exam)
       .subscribe(success => this.router.navigateByUrl('/exam'));
---
+++ Testing_System/src/main/java/com/cmcglobal/service/CategoryService.java
@@ -0,0 +1,33 @@
+/**
+ * Project name: Testing_System
+ * Package name: com.cmcglobal.service
+ * File name: CategoryService.java
+ * Author: ptphuong.
+ * Created date: Feb 15, 2019
+ * Created time: 1:16:43 PM
+ */
+
+package com.cmcglobal.service;
+
+import java.util.List;
+
+import com.cmcglobal.entity.Category;
+
+/*
+ * @author ptphuong.
+ * Created date: Feb 15, 2019
+ * Created time: 1:16:43 PM
+ * Description: TODO - 
+ */
+public interface CategoryService {
+
+    /**
+     * Author: ptphuong.
+     * Created date: Feb 15, 2019
+     * Created time: 1:16:52 PM
+     * Description: TODO - .
+     * @return
+     */
+    List<Category> findAll();
+
+}
