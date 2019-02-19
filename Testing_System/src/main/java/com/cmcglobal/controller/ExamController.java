08f09979143783fadbd6fcd22059c57a6c9488d6
hai95 <conghaibk95@gmail.com>
2019-02-19 17:13:20 +0700

    ok

--- Testing_System/src/main/java/com/cmcglobal/controller/ExamController.java
+++ Testing_System/src/main/java/com/cmcglobal/controller/ExamController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,28 +21,34 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cmcglobal.entity.Exam;
+import com.cmcglobal.entity.User;
 import com.cmcglobal.repository.CategoryRepository;
 import com.cmcglobal.service.ExamService;
 import com.cmcglobal.utils.ExportExamPDF;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/exam")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class ExamController {
-  @Autowired
-  ExamService examService;
-  @Autowired
-  CategoryRepository cate;
+	@Autowired
+	ExamService examService;
+	@Autowired
+	CategoryRepository cate;
 
-  @PostMapping(value = "/create")
-  public void postExam(@RequestBody Exam exam) {
-    examService.createExam(exam);
-  }
+	@PostMapping(value = "/create")
+	public void postExam(@RequestBody Exam exam) {
+		examService.createExam(exam);
+	}
 
-  @GetMapping(value = "/listExams")
-  public List<Exam> listExam() {
-    return examService.findAll();
-  }
+	@GetMapping(value = "/listExams")
+	public List<Exam> listExam() {
+		return examService.findAll();
+	}
 
 	@RequestMapping(value = "listExams/pagination", method = RequestMethod.GET)
 	private List<Exam> getPageExam(
@@ -108,7 +115,7 @@ public class ExamController {
 				sortable = Sort.by("createAt").descending();
 			}
 			break;
-			//sort theo trường fullname của user create_by
+		// sort theo trường fullname của user create_by
 		case ("userCreated"):
 			if (("asc").equals(sortOrder.toLowerCase())) {
 				sortedBy = PageRequest.of(page, size);
@@ -122,52 +129,103 @@ public class ExamController {
 		}
 		sortedBy = PageRequest.of(page, size, sortable);
 		return examService.pageExam(sortedBy);
-  }
+	}
 
-  @GetMapping(value = "/export/{id}")
-  public ModelAndView handlereport(@PathVariable("id") String id) {
-    try {
-      Exam exam = examService.findByID(id);
-      return new ModelAndView(new ExportExamPDF(), "exam", exam);
-    } catch (Exception e) {
-      return null;
-    }
-  }
+	@GetMapping(value = "/export/{id}")
+	public ModelAndView handlereport(@PathVariable("id") String id) {
+		try {
+			Exam exam = examService.findByID(id);
+			return new ModelAndView(new ExportExamPDF(), "exam", exam);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
-  @GetMapping(value = "/{id}")
-  public Exam getExam(@PathVariable("id") String id) {
-    /*
-     * cate.delete(cate.getOne(1)); cate.deleteAll();
-     */
-    return examService.findByID(id);
-  }
+	@GetMapping(value = "/{id}")
+	public Exam getExam(@PathVariable("id") String id) {
+		/*
+		 * cate.delete(cate.getOne(1)); cate.deleteAll();
+		 */
+		return examService.findByID(id);
+	}
 
-  @PutMapping(value = "/approve")
-  public ResponseEntity<String> approveExam(@RequestBody Exam exam) {
-    boolean success = examService.approveExam(exam.getExamId());
-    if (success)
-      return ResponseEntity.ok("Ok");
-    return ResponseEntity.ok("Not ok");
-  }
+	@PutMapping(value = "/approve")
+	public ResponseEntity<String> approveExam(@RequestBody Exam exam) {
+		boolean success = examService.approveExam(exam.getExamId());
+		if (success)
+			return ResponseEntity.ok("Ok");
+		return ResponseEntity.ok("Not ok");
+	}
 
-  @PutMapping(value = "/remove-question")
-  public ResponseEntity<String> removeQuestion(@RequestBody Exam exam) {
-    boolean success = examService.removeQuestion(exam);
-    if (success)
-      return ResponseEntity.ok("Ok");
-    return ResponseEntity.ok("Not ok");
-  }
+	@PutMapping(value = "/remove-question")
+	public ResponseEntity<String> removeQuestion(@RequestBody Exam exam) {
+		boolean success = examService.removeQuestion(exam);
+		if (success)
+			return ResponseEntity.ok("Ok");
+		return ResponseEntity.ok("Not ok");
+	}
 
-  @PostMapping(value = "/add-question")
-  public ResponseEntity<String> addQuestion(@RequestBody Exam exam) {
-    
-    examService.addListQuestion(exam);
-    return ResponseEntity.ok("Ok");
-  }
+	@PostMapping(value = "/add-question")
+	public ResponseEntity<String> addQuestion(@RequestBody Exam exam) {
 
-  @PostMapping(value = "/random-question")
-  public ResponseEntity<String> randomQuestion(@RequestBody Exam exam) {
-    examService.randomQuestion(exam.getExamId(), exam.getNumberOfQuestion());
-    return ResponseEntity.ok("Ok");
-  }
+		examService.addListQuestion(exam);
+		return ResponseEntity.ok("Ok");
+	}
+
+	@PostMapping(value = "/random-question")
+	public ResponseEntity<String> randomQuestion(@RequestBody Exam exam) {
+		examService.randomQuestion(exam.getExamId(), exam.getNumberOfQuestion());
+		return ResponseEntity.ok("Ok");
+	}
+
+//	@GetMapping("/{examId}")
+//	public Exam getOne(@PathVariable String examId) {
+//		return examService.getOne(examId);
+//	}
+
+	@PutMapping("/update/update-common/{examId}")
+	public Exam updateCommon(@RequestBody Exam exam, @PathVariable String examId) {
+		Exam ex = examService.getOne(examId);
+		if (ex != null) {
+			ex.setTitle(exam.getTitle());
+			ex.setCategory(exam.getCategory());
+			ex.setDuration(exam.getDuration());
+			ex.setNumberOfQuestion(exam.getNumberOfQuestion());
+			ex.setNote(exam.getNote());
+			ex.setStatus(exam.getStatus());
+			examService.update(ex);
+			System.out.println(exam.getCategory().getCategoryName());
+		}
+		return ex;
+	}
+
+	@PostMapping("/import-excel-file")
+	public ResponseEntity<String> readExcelFile(@RequestParam("multipartFile") MultipartFile multipartFile) {
+
+		File file = new File("files");
+		String pathToSave = file.getAbsolutePath();
+		System.out.println(pathToSave);
+
+		File fileTranfer = new File(pathToSave + "/" + multipartFile.getOriginalFilename());
+		try {
+			multipartFile.transferTo(fileTranfer);
+		} catch (IllegalStateException e) {
+		} catch (IOException e) {
+		}
+		System.out.println(multipartFile.getOriginalFilename());
+
+		List<Exam> listExam = examService.readExcel(fileTranfer.toString());
+		if (listExam.size() == 0) {
+			return ResponseEntity.status(HttpStatus.OK).body("not Ok");
+		}
+		for (Exam exam : listExam) {
+			exam.setExamId(examService.createId());
+			User user = new User();
+			user.setUserId(1);
+			exam.setUserCreated(user);
+			exam.setStatus("Draft");
+			examService.insert(exam);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body("Ok");
+	}
 }
